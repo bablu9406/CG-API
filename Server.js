5527de3abca75de1a58b0fa1c7cc130ae0135a16
bablu9406 <krishnabablu22@gmail.com>
2025-09-02 22:57:29 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,41 +1,18 @@
-// server.js
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
+import express from "express";
+import cors from "cors";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// MongoDB से कनेक्ट करें
-mongoose.connect("YOUR_MONGODB_URI_HERE", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-// एक Schema और Model बनाएं
-const UserSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-});
-const User = mongoose.model("User", UserSchema);
-
-// Routes
+// Home route
 app.get("/", (req, res) => {
-  res.send("API is working 🚀");
+  res.send("API is working!");
 });
 
-// नया यूजर सेव करने का API
-app.post("/users", async (req, res) => {
-  const user = new User(req.body);
-  await user.save();
-  res.send(user);
-});
-
-// सभी यूजर लाने का API
-app.get("/users", async (req, res) => {
-  const users = await User.find();
-  res.send(users);
+// Example API route
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello from API!" });
 });
 
 const PORT = process.env.PORT || 3000;
